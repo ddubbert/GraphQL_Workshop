c6bde08488a360087618b2df065b1afe8da191c2
ddubbert <dennis.dubbert@smail.th-koeln.de>
2019-05-04 14:20:52 +0200

    prepared tasks and copied solutions to solution folder

---
+++ Solutions/tasks.js
@@ -0,0 +1,57 @@
+const OrderState = require('../utils/enums/OrderState') 
+
+const productDB = require('../utils/databases/product.db')
+const userDB = require('../utils/databases/user.db')
+const orderDB = require('../utils/databases/order.db')
+
+const sharedOrderResolvers = {
+    product: (parent, _args, _context, _info) => {
+        return productDB.getProductById(parent.productId)
+    },
+    customer: (parent, _args, _context, _info) => {
+        return userDB.getUserById(parent.customerId)
+    }, 
+    producer: (parent, _args, _context, _info) => {
+        return userDB.getUserById(parent.producerId)
+    },
+}
+
+module.exports = {
+    Query: {
+        orders: (_parent, args, _context, _info) => {
+            const { producerId } = args
+            const orders = orderDB.getOrdersForProducer(producerId)
+
+            return (orders.length > 0) ? orders : null
+        }
+    },
+    Mutation: {
+        createOrder: (_parent, args, _context, _info) => {
+            const { orderInput } = args
+            const keys = Object.keys(orderInput)
+
+            if (!productDB.isProduct(orderInput.productId)) throw new Error('Product not found.')
+            if (!userDB.isUser(orderInput.customerId)) throw new Error('Product not found.')
+            if (keys.length !== 5) throw new Error('Input is not valid.')
+
+            return orderDB.createOrder(orderInput)
+        }
+    },
+    Order: {
+        __resolveType(order) {
+            if (order.shipping_address) return 'MailOrder'
+            else if (order.pickup_date) return 'PickupOrder'
+            throw new Error('Could not identify order type.')
+        },
+    },
+    MailOrder: {
+        ...sharedOrderResolvers
+    },
+    PickupOrder: {
+        ...sharedOrderResolvers
+    },
+    OrderState: {
+        PAYED: OrderState.PAYED,
+        OPEN: OrderState.OPEN
+    }
+}
